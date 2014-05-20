51f3ac52975d198bcacb8c28340ff6b6d09a3a95
sdj38 <sjones38@live.com>
2014-05-19 22:39:40 -0600

    Added sound

---
+++ Worm_Bros/src/SoundTest.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SoundTest  {
+	public static final AudioClip BITE = Applet.newAudioClip(SoundTest.class.getResource("Splat.wav"));
+}
